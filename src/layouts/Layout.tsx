eb8e476b21b43ba9a6e36a89ac0e4ea4a656efbd
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-06-22 22:15:15 +0200

    Replace div by header and footer & mx on footer

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -10,28 +10,28 @@ function Layout() {
   return (
     <>
 <div className="flex flex-col items-center h-screen justify-between">
-  <div className="header">
+  <header className="header mb-12 h-10">
     
-  </div>
-  <div className="content mt-0 mn-auto">
+  </header>
+  <div className="content mt-0 mb-auto justify-center">
   <div>
     Coming home soon, Japan ...
   </div>
   <div className="flex items-center">
-    <img src={Flight} className="w-10 h-10 mr-2" />
+    <img src={Flight} className="mr-2" />
     <img src={Home} className="w-10 h-10" />
   </div>
   <DisplayCountdown />
   </div>
-  <div className="footer mb-8">
-  <div className="flex items-center mt-4">
-    <img src={Github} className="w-6 h-6 mr-2" />
-    <div>
+  <footer className="footer mb-8">
+  <div className="flex items-center justify-between mt-4">
+    <img src={Github} className="h-20 mx-10" />
+    <div className="mx-12">
       Chou & Dou
     </div>
-    <img src={Us} className="w-6 h-6 ml-2" />
+    <img src={Us} className="h-20 mx-12" />
   </div>
-  </div>
+  </footer>
 </div>
     </>
   );
