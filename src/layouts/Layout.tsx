0122aec0323bf5a0fbfb0d3043d3328ec0bfb15c
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-06-23 23:15:40 +0200

    Scale up the shisa logo

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -9,30 +9,30 @@ function Layout() {
 
   return (
     <>
-<div className="flex flex-col items-center h-screen justify-between">
-  <header className="header mb-12 h-10">
-    
-  </header>
-  <div className="content mt-0 mb-auto justify-center">
-  <div>
-    Coming home soon, Japan ...
-  </div>
-  <div className="flex items-center">
-    <img src={Flight} className="mr-2" />
-    <img src={Home} className="w-10 h-10" />
-  </div>
-  <DisplayCountdown />
-  </div>
-  <footer className="footer mb-8">
-  <div className="flex items-center justify-between mt-4">
-    <img src={Github} className="h-20 mx-10" />
-    <div className="mx-12">
-      Chou & Dou
-    </div>
-    <img src={Us} className="h-20 mx-12" />
-  </div>
-  </footer>
-</div>
+      <div className="flex flex-col items-center h-screen justify-between">
+        <header className="header mb-12 h-10">
+
+        </header>
+        <div className="content mt-0 mb-auto">
+          <div>
+            Coming home soon, Japan ...
+          </div>
+          <div className="flex self-start ">
+            <img src={Flight} className="mr-6" />
+            <img src={Home} className="h-10" />
+          </div>
+          <DisplayCountdown />
+        </div>
+        <footer className="footer mb-8">
+          <div className="flex items-center justify-between mt-4">
+            <img src={Github} className="h-10 mx-10" />
+            <div className="mx-12">
+              Chou & Dou
+            </div>
+            <img src={Us} className="h-24 mx-10" />
+          </div>
+        </footer>
+      </div>
     </>
   );
 }
